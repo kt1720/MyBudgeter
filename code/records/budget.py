508b3cf82aa14c51ef2c516505c411975312ef9c
Jacob Rosen <jimr95>
2023-11-27 13:21:56 -0800

    reorganized structure

--- code/records/budget.py
+++ code/records/budget.py
@@ -2,14 +2,9 @@ class Budget(object):
     def __init__(self, n_categories=1) -> None:
         self.n_categories = n_categories
         self.budget = self.new_budget(self.n_categories)
-        self.spending = self.get_spending()
     
     def new_budget(n):
         # get user to give n category names and limits
-        pass
-
-    def get_spending():
-        # need a way to calculate user spending
         pass
 
     def check_budget():
--- code/users/household.py
+++
@@ -1,8 +0,0 @@
-from users import User
-
-class Household(User):
-    def __init__(self, name) -> None:
-        super().__init__(name)
-        pass
-
-    
