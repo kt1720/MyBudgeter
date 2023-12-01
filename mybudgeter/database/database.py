22de4ba0277b1ecd75b5ba0435ef3e1f97bbca23
Jacob Rosen <jimr95>
2023-12-01 13:33:49 -0800

    created db inheritance

--- mybudgeter/database/database.py
+++ mybudgeter/database/database.py
@@ -5,26 +5,26 @@ import sqlite3
 class Database(object):
     def __init__(self, file_path, db=None) -> None:
         if db:
-            self.__connect(db)
+            self.connect(db)
         else:
-            self.__create_db(file_path)
+            self.create_db(file_path)
     
-    def __connect(self, db):
-        self.__cnx = sqlite3.connect(db)
-        self.__cur = self.__cnx.cursor()
+    def connect(self, db):
+        self.cnx = sqlite3.connect(db)
+        self.cur = self.cnx.cursor()
 
-    def __close(self):
-        self.__cnx.close()
+    def close(self):
+        self.cnx.close()
 
     def __del__(self):
-        self.__close()
+        self.close()
 
-    def __query(self, query, args=None):
+    def query(self, query, args=None):
         try:
             if args:
-                self.__cur.execute(query, args)
+                self.cur.execute(query, args)
             else:
-                self.__cur.execute(query)
+                self.cur.execute(query)
         except sqlite3.Error as err: 
             print(err)
 
