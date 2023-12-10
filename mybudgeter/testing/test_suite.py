a12335520fb2518191eecffad328014d7e132097
Jacob Rosen <jimr95>
2023-12-09 18:15:08 -0800

    made test suite more efficient

--- mybudgeter/testing/test_suite.py
+++ mybudgeter/testing/test_suite.py
@@ -6,16 +6,9 @@ def my_suite():
 	suite = unittest.TestSuite
 	result = unittest.TestResult()
 	# add test for budget
-	suite.addTest(TestBudget('test_add_category'))
-	suite.addTest(TestBudget('test_modify'))
-	suite.addTest(TestBudget('test_delete'))
-	suite.addTest(TestBudget('test_check'))
-	suite.addTest(TestBudget('test_get_categories'))
+	suite.addTest(unittest.makeSuite(TestBudget))
 	# add test for transactions
-	suite.addTest(TestTransactions('test_add_transaction'))
-	suite.addTest(TestTransactions('test_modify_transaction'))
-	suite.addTest(TestTransactions('test_delete_transaction'))
-	suite.addTest(TestTransactions('test_get_n_transactions'))
+	suite.addTest(unittest.makeSuite(TestTransactions))
 
 	runner = unittest.TextTestRunner() 
 	print(runner.run(suite))
